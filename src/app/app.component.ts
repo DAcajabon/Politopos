3be346bf9faf84b4a06876c7b1091703df352334
DAcajabon <diego.acajabon@gmail.com>
2018-12-03 15:42:02 -0600

    Campos y limpiar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,12 @@ import { Component, Input, ElementRef, AfterViewInit, ViewChild } from '@angular
 })
 export class AppComponent implements AfterViewInit{
 
-  lados: number;
+  lados: number = null;
   posiciones: number[];
   centroX: number;
   centroY: number;
+  ancho: number;
+  alto: number;
   title = 'Politopes';
 
   @ViewChild('canvas') public canvas: ElementRef;
@@ -20,7 +22,7 @@ export class AppComponent implements AfterViewInit{
 
   ngAfterViewInit(): void {
     const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
-    canvasEl.setAttribute('width', '750px');
+    canvasEl.setAttribute('width', '600px');
     canvasEl.setAttribute('height', '480px');
     this.cx = canvasEl.getContext('2d');
     this.cx.lineWidth = 3;
@@ -28,14 +30,30 @@ export class AppComponent implements AfterViewInit{
     this.cx.strokeStyle = '#000';
     this.centroX = canvasEl.width / 2;
     this.centroY = canvasEl.height / 2;
+    this.circulo();
   }
 
   dibujar(){
     this.cx.beginPath();
-    this.cx.arc(this.centroX, this.centroY, 230, 0, 2 * Math.PI);
     this.cx.moveTo(this.centroX, this.centroY);
     this.cx.lineTo(this.centroX, this.centroY + 50);
     this.cx.stroke();
+  }
+
+  calcular(){
+    //https://en.wikipedia.org/wiki/List_of_regular_polytopes_and_compounds
+  }
+
+  limpiar(){
+    this.cx.clearRect(0, 0, 600, 480);
+    this.circulo();
+  }
+
+  circulo(){
+    this.cx.beginPath();
+    this.cx.arc(this.centroX, this.centroY, 230, 0, 2 * Math.PI);
+    this.cx.stroke();
+    this.cx.closePath();
   }
 
 }
